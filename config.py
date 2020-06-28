1212e2857ba624601ecbd88bf52c51d9c0287620
vml <iam@codefather.ru>
2020-06-29 00:41:26 +0300

    накопленное

---
+++ config.py
@@ -0,0 +1,8 @@
+﻿# -*- coding: utf-8 -*-
+# Это токен телеграма
+token = '454777783:AAEz6Xr9lVXZIl5z9MS-LUessRcWsR5-3WM'
+
+socks5 = 'socks5://cgnsock:aqE2maG2SnAH4c@37.59.236.185:8480'
+
+database_name = 'quest.sqlite'  # Файл с базой данных
+shelve_name = 'shelve.db'  # Файл с хранилищем
